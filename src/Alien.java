35a1a7c03e8a2cfdc65d6409d15d9bad85067d2a
RealUnknownDeath <91622430+RealUnknownDeath@users.noreply.github.com>
2026-01-14 08:39:42 -0500

    Done

--- src/Alien.java
+++ src/Alien.java
@@ -1,29 +1,28 @@
 public class Alien {
-
     private int energyLevel;
     private int hostilityIndex;
 
-    public Alien(int energyLevel, int paramHostilityIndex) {
+    public Alien(int energyLevel, int hostilityIndex){
         this.energyLevel = energyLevel;
-        this.hostilityIndex = paramHostilityIndex;
-    } // end of constructor
-
-    public int getEnergyLevel() {
-        return energyLevel;
-    }
-    public void setEnergyLevel(int paramEnergy) {
-        this.energyLevel = paramEnergy;
-    }
-    public int getHostilityIndex() {
-        return hostilityIndex;
-    }
-    public void setHostilityIndex(int paramHostility) {
-        hostilityIndex = paramHostility;
-        // this.hostilityIndex = paramHostility
-    }
-    public void printInfo() {
-        System.out.println("Energy: " + energyLevel +
-                ", Hostility: " + hostilityIndex);
+        this.hostilityIndex = hostilityIndex;
     }
 
+    public int getEnergyLevel(){
+        return this.energyLevel;
+    }
+    public int getHostilityIndex(){
+        return this.hostilityIndex;
+    }
+
+    public void setEnergyLevel(int energyLevel) {
+        this.energyLevel = energyLevel;
+    }
+    public void setHostilityIndex(int hostilityIndex) {
+        this.energyLevel = hostilityIndex;
+    }
+
+    public void printInfo(){
+        System.out.println("Energy Level: " + this.energyLevel);
+        System.out.println("Hostility Index: " + this.hostilityIndex);
+    }
 }
--- src/StarshipExplorer.java
+++ src/StarshipExplorer.java
@@ -1,73 +1,64 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class StarshipExplorer {
 
     // variable declaration section
     private String alienLanguage = "Vrlnq*T xm!ow b*aF#nmpq*k^s jfy*e* *m&xlqw*e tT@br* lm*t oT%*ovnzr* oY#rwqs*y uo@L*oe^t aE!wqlm*u*r aD@rzxs* eB&vnyp Rlkzx*l W tnl@qp*e eXm*a#kyr*d aM^yplw o*e&mnbs*r eI!k*,srn* pC#txry*B cO@pb*elm Hlmpq*y P tn!*ojlwl rE#kxtm*n eF^l*cqrx tO&wtnq*e fC*!xtpm oK#xrnl nT@klpm cT@vrqp";
-    public Alien[] aliens;
+
+    public Alien[] aliens = new Alien[50];
 
     public static void main(String[] args) {
-        StarshipExplorer ship = new StarshipExplorer();
+        System.out.println("Hello World! Good luck on your exams!");
+        StarshipExplorer test = new StarshipExplorer();
+
     }
 
-    public StarshipExplorer() {
-        System.out.println("Hello world! Good luck on your exams!");
-        aliens = new Alien[50];
-        for (int i = 0; i < 50; i++) {
-            // Math.random() gives a decimal b/n 0-1
-            int energy = (int) (Math.random() * 75 + 1);
-            int hostility = (int) (Math.random() * 101);
-            aliens[i] = new Alien(energy, hostility);
+    public StarshipExplorer(){
+        int num1 = -1;
+        int num2 = -1;
+        for(int q = 0; q < 50; q++){
+            while(num1 < 0 || num1 > 75){
+                num1 = (int)Math.round(Math.random()*10)*10 + 1;
+            }
+            while(num2 <= 0 || num2 > 100){
+                num2 = (int)Math.round(Math.random()*10)*10;
+            }
+
+            aliens[q] = new Alien(num1, num2);
         }
+        System.out.println(descipher(alienLanguage));
         displayAliens();
-        boolean c = checkEnergy();
-        System.out.println("It is " + c + " that two aliens have the same energy level.");
-        decipher(alienLanguage);
-    } // end of constructor
-    public void displayAliens() {
-        for (int i = 0; i < aliens.length; i++) {
+    }
+
+    public void displayAliens(){
+        for(int i = 0; i < 50; i++){
             aliens[i].printInfo();
         }
     }
 
-    public boolean checkEnergy() {
-        // check whether at least two energyLevel
-        // values in the aliens array are equal
-        boolean sameEnergy = false;
+    public boolean checkEnergyLevels(Alien[] list){
+        ArrayList<Integer> checker = new ArrayList<>();
 
-        // check whether aliens[0] energy is the same as anyone else
-        for (int x = 0; x < aliens.length; x++) {
-            for (int i = 0; i < aliens.length; i++) {
-                if (aliens[i].getEnergyLevel() == aliens[x].getEnergyLevel() && (i != x)) {
-                    sameEnergy = true;
-//                    System.out.println("aliens[" + i + "] energy: " + aliens[i].getEnergyLevel());
-//                    System.out.println("aliens[" + x + "] energy: " + aliens[x].getEnergyLevel());
-                }
+        for(int i = 0; i < list.length; i++){
+            if(checker.contains(list[i].getEnergyLevel())){
+                return true;
+            }
+            checker.add(list[i].getEnergyLevel());
+        }
+
+        return false;
+    }
+
+    public String descipher(String s){
+        String returner = "";
+        for(int i = 0; i < s.toCharArray().length; i++){
+            if(s.charAt(i) == '*') {
+                returner = returner + (s.substring(i+1, i + 2));
             }
         }
-
-        // return true if at least two alien energy levels are equal.
-        // Otherwise, return false
-        if (sameEnergy == true) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    } // end of checkEnergy
-
-    public String decipher(String languageSample) {
-        String decodedString = "";
-        // find each character that appears directly after a “*” character
-        System.out.println(languageSample);
-        // indexOf, contains, substring
-        int al = languageSample.indexOf("*") + 1;
-        while (al != 0) {
-            String sub = languageSample.substring(al, al + 1);
-            decodedString += sub;
-            al = languageSample.indexOf("*", al) + 1;
-        }
-        System.out.println(decodedString);
-
-        return decodedString;
+        return returner;
     }
+
 
 }
